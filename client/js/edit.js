bbc9bb9266cdaa1a832363b22b79d5e5b3c1f220
Tenari the Great <djz24@case.edu>
2013-09-24 12:15:14 -0400

    add: scene edit set

--- client/js/edit.js
+++ client/js/edit.js
@@ -1,3 +1,15 @@
+Template.edit.action_type = function(type){
+  return this.type == type;
+};
+Template.edit.scene_actions = function(){
+  return Scenes.findOne(Session.get('scene id')).actions;
+};
+Template.edit.scene_location = function(){
+  return Scenes.findOne(Session.get('scene id')).location;
+};
+Template.edit.editing = function(){
+  return Session.get('editing');
+};
 Template.edit.scene_objects = function(chapterId){
   var scenes = [];
   var chapter = Chapters.findOne(chapterId.concat(""));
@@ -15,6 +27,8 @@ Template.edit.real_chapter = function(chapterId){
 Template.edit.chapter_title = function(chapterId){
   return Chapters.findOne(chapterId).title;
 };
+
+// EVENTS
 Template.edit.events({
   'click #addChapter':function(){
     var chapterId = Chapters.insert({
@@ -46,6 +60,38 @@ Template.edit.events({
     Scenes.update(this._id, {
       $set: { location: $(e.target).val().trim() }
     });
-    console.log(this);
+  },
+  'click .editScene': function(e){
+    Session.set('editing',true);
+    Session.set('scene id',this._id);
+  },
+  'click #saveScene': function(e){
+    Session.set('editing',false);
+  },
+  'click #addActionSpeech':function(e){
+    Scenes.update(Session.get('scene id'),{
+      $push: {actions: {
+        type: "speech",
+        speaker: "NAME",
+        speech: "Blah blah blah.",
+        animations:[]
+      }}
+    });
+  },
+  'click #addActionChoice':function(e){
+    Scenes.update(Session.get('scene id'),{
+      $push: {actions: {
+        type: "choice",
+        options: []
+      }}
+    });
+  },
+  'click #addActionVisual':function(e){
+    Scenes.update(Session.get('scene id'),{
+      $push: {actions: {
+        type: "visual",
+        animations:[]
+      }}
+    });
   }
 });
